473baf6315a6c8205f42d31d34931b059db5e174
Ammy572 <141164113+Ammy572@users.noreply.github.com>
2023-09-30 18:51:10 +0530

    Add files via upload

---
+++ 22.2023/codes/code.py
@@ -0,0 +1,10 @@
+# Calculate the method of moments estimator for lambda symbolically
+def estimate_lambda_symbolic():
+    return "sqrt(Y_bar / 2)"
+
+# Get the symbolic expression for lambda in terms of Y_bar
+lambda_expression = estimate_lambda_symbolic()
+
+# Print the symbolic expression
+print("Estimated Lambda in terms of Y_bar:", lambda_expression)
+
